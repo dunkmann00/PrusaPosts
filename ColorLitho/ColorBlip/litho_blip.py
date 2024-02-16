918060f720e1b9a3a24360c106fcc5edc3d83b6d
George Waters <gwatersdev@gmail.com>
2024-02-15 19:35:46 -0500

    Rewrite ColorBlip
    This includes a rewrite of ColorBlip to improve the shortcomings of the
    original implementation. This one is much better, but still does not
    support some common scenarios like wipes, and z-hops.

--- ColorLitho/ColorBlip/litho_blip.py
+++ ColorLitho/ColorBlip/litho_blip.py
@@ -5,21 +5,40 @@ from math import sqrt
 from collections import namedtuple
 import argparse, re
 
+# G-Code
+#   |
+#   |--> Pre-Print   Print   Post-Print
+#                      |
+#                      |-->  Pre-Print   Layers
+#                                          |
+#                                          |--> Lines
 
 BLIP_REMOVER_ENABLED = "; BLIP_REMOVER_ENABLED"
 BLIP_REMOVER_DISABLED = "; BLIP_REMOVER_DISABLED"
+COLOR_CHANGE_ID = "M600"
 LAYER_CHANGE_ID = ";LAYER_CHANGE"
 RETRACT_BEFORE_TRAVEL_ID = "; retract_before_travel "
+FILAMENT_RETRACT_BEFORE_TRAVEL_ID = "; filament_retract_before_travel "
 RETRACT_LENGTH_ID = "; retract_length "
+FILAMENT_RETRACT_LENGTH_ID = "; filament_retract_length "
+RETRACT_LAYER_CHANGE = "; retract_layer_change "
+FILAMENT_RETRACT_LAYER_CHANGE = "; filament_retract_layer_change "
 RETRACT_SPEED_ID = "; retract_speed "
+FILAMENT_RETRACT_SPEED_ID = "; filament_retract_speed "
 RETRACT_LIFT_ID = "; retract_lift "
 TRAVEL_SPEED_ID = "; travel_speed "
 TRAVEL_SPEED_Z_ID = "; travel_speed_z "
 NOZZLE_DIAMETER_ID = "; nozzle_diameter"
+WIPE_ID = "; wipe "
+FILAMENT_WIPE_ID = "; filament_wipe "
 WIPE_START = ";WIPE_START"
 WIPE_END = ";WIPE_END"
 M204_ID = "M204 P"
+START_PRINTING_OBJECT_ID = "; printing object"
+STOP_PRINTING_OBJECT_ID = "; stop printing object"
 TYPE_ID = ";TYPE:"
+Z_HOP_ID = "; retract_lift "
+FILAMENT_Z_HOP_ID = "; filament_retract_lift "
 
 g1_line = re.compile("^G1 (?:(?:X([0-9]*\.?[0-9]*) *)|(?:Y([0-9]*\.?[0-9]*) *)|(?:Z([0-9]*\.?[0-9]*) *)|(?:E(-?[0-9]*\.?[0-9]*) *)|(?:F([0-9]+) *))+$")
 G1 = namedtuple("G1", ["x", "y", "z", "e", "f"])
@@ -62,6 +81,245 @@ class Point(namedtuple("Point", ["x", "y", "z"])):
     def is_xy_equal(self, point):
         return self.x == point.x and self.y == point.y
 
+class GCodeConfig:
+    def __init__(self, gcode_lines):
+        self.travel_speed = int(get_value_for_id(TRAVEL_SPEED_ID, gcode_lines)) * 60     # convert to speed/min
+        self.travel_speed_z = int(get_value_for_id(TRAVEL_SPEED_Z_ID, gcode_lines)) * 60 # convert to speed/min
+        self.nozzle_diameter = float(get_value_for_id(NOZZLE_DIAMETER_ID, gcode_lines).split(",")[0])
+        # if wipe_threshold is None:
+        self.wipe_threshold = float(get_value_for_id((FILAMENT_RETRACT_BEFORE_TRAVEL_ID, RETRACT_BEFORE_TRAVEL_ID), gcode_lines).split(",")[0])
+
+        self.retract_len = float(get_value_for_id((FILAMENT_RETRACT_LENGTH_ID, RETRACT_LENGTH_ID), gcode_lines).split(",")[0])
+        self.retract_speed = int(get_value_for_id((FILAMENT_RETRACT_SPEED_ID, RETRACT_SPEED_ID), gcode_lines).split(",")[0]) * 60 # convert to speed/min
+        self.retract_layer_change = int(get_value_for_id((FILAMENT_RETRACT_LAYER_CHANGE, RETRACT_LAYER_CHANGE), gcode_lines).split(",")[0]) == 1
+
+        self.wipe = int(get_value_for_id((FILAMENT_WIPE_ID, WIPE_ID), gcode_lines).split(",")[0]) == 1
+        self.z_hop = float(get_value_for_id((FILAMENT_Z_HOP_ID, Z_HOP_ID), gcode_lines).split(",")[0])
+
+class GCode:
+    def __init__(self, gcode_lines):
+        self.config = GCodeConfig(gcode_lines)
+        if self.config.wipe:
+            raise RuntimeError("ColorBlip does not currently support 'Wipe while retracting'.")
+        if self.config.z_hop > 0:
+            raise RuntimeError("ColorBlip does not currently support 'Lift Height' (aka Z-Hop).")
+        pre, post, layers = self.process_gcode_lines(gcode_lines)
+        self._pre_print = pre
+        self._post_print = post
+        self.layers = layers
+
+    @property
+    def pre_print_gcode_lines(self):
+        return self._pre_print
+
+    @property
+    def print(self):
+        gcode_lines = []
+        for layer in self.layers:
+            gcode_lines += layer.gcode_lines()
+        return gcode_lines
+
+    @property
+    def post_print_gcode_lines(self):
+        return self._post_print
+
+    def gcode_lines(self):
+        return self._pre_print + self.print + self._post_print
+
+    def process_gcode_lines(self, gcode_lines):
+        current_point = Point.undefined()
+        blip_remover_enabled = False
+
+        pre_end_idx = 0
+        for i, line in enumerate(gcode_lines):
+            if line == BLIP_REMOVER_ENABLED:
+                blip_remover_enabled = True
+            elif line == BLIP_REMOVER_DISABLED:
+                blip_remover_enabled = False
+            elif line == LAYER_CHANGE_ID:
+                pre_end_idx = i
+                break
+            else:
+                g1 = parse_line(line)
+                if g1 is not None:
+                    current_point = current_point.updated_with_g1_move(g1)
+
+        post_start_idx = len(gcode_lines)
+        for i, line in enumerate(reversed(gcode_lines)):
+            i_rev = len(gcode_lines) - i - 1
+            if line.startswith(STOP_PRINTING_OBJECT_ID):
+                post_start_idx = i_rev + 1
+                break
+
+        layers = []
+        layer_start_idx = None
+        for i, line in enumerate(gcode_lines[pre_end_idx:post_start_idx], start=pre_end_idx):
+            if line == BLIP_REMOVER_ENABLED:
+                blip_remover_enabled = True
+            elif line == BLIP_REMOVER_DISABLED:
+                blip_remover_enabled = False
+            elif line == LAYER_CHANGE_ID:
+                if layer_start_idx is not None:
+                    layer = Layer(gcode_lines[layer_start_idx:i], self.config, current_point, blip_remover_enabled)
+                    current_point = layer.end_point
+                    layers.append(layer)
+                layer_start_idx = i
+        # Handle the last layer
+        layer = Layer(gcode_lines[layer_start_idx:post_start_idx], self.config, current_point, blip_remover_enabled)
+        current_point = layer.end_point
+        layers.append(layer)
+
+        return gcode_lines[:pre_end_idx], gcode_lines[post_start_idx:], layers
+
+class Layer:
+    def __init__(self, gcode_lines, config, start_point, enabled):
+        self.config = config
+        pre, lines, end_point = self.process_gcode_lines(gcode_lines, start_point, enabled)
+        self._pre_print = pre
+        self.lines = lines
+        self.start_point = start_point
+        self.end_point = end_point
+        self.enabled = enabled
+
+    @property
+    def has_layer_color_change(self):
+        return COLOR_CHANGE_ID in self._pre_print
+
+    @property
+    def pre_print_gcode_lines(self):
+        return self._pre_print
+
+    @property
+    def print(self):
+        gcode_lines = []
+        if not self.enabled:
+            for line in self.lines:
+                gcode_lines += line.gcode_lines()
+            return gcode_lines
+        valid_lines = [line for line in self.lines if line.line_length > self.config.nozzle_diameter]
+        global lines_removed
+        lines_removed += len(self.lines) - len(valid_lines)
+        current_type = None
+        next_line = None
+        for i, line in enumerate(valid_lines):
+            next_line = valid_lines[i+1] if i+1 < len(valid_lines) else None
+            if line.type is not None and line.type != current_type:
+                gcode_lines.append(line.type)
+                current_type = line.type
+            if i == 0 and self.config.retract_layer_change:
+                line.add_deretraction()
+                if self.has_layer_color_change:
+                    line.add_retraction(0) # This may not be needed >= PrusaSlicer 2.7.2
+                line.add_start_feedrate()
+            if next_line is not None and line.end_point.xy_dist(next_line.start_point) > self.config.wipe_threshold:
+                line.add_retraction()
+                next_line.add_deretraction()
+            gcode_lines += line.gcode_lines()
+        return gcode_lines
+
+    def gcode_lines(self):
+        return self._pre_print + self.print
+
+    def process_gcode_lines(self, gcode_lines, start_point, enabled):
+        pre_end_idx = 0
+        current_point = start_point
+        for i, line in enumerate(gcode_lines):
+            g1 = parse_line(line)
+            if g1 is not None:
+                current_point = current_point.updated_with_g1_move(g1)
+            elif line.startswith(START_PRINTING_OBJECT_ID):
+                pre_end_idx = i + 1
+                break
+
+        lines = []
+        line_start_idx = pre_end_idx
+        line_start_point = start_point
+        current_type = None
+        for i, line in enumerate(gcode_lines[pre_end_idx:], start=pre_end_idx):
+            g1 = parse_line(line)
+            if g1 is not None:
+                new_point = current_point.updated_with_g1_move(g1)
+                if g1.e is None and not current_point.is_xy_equal(new_point): # Travel move found
+                    line = Line(gcode_lines[line_start_idx:i], self.config, line_start_point, current_point, current_type, enabled)
+                    lines.append(line)
+                    line_start_idx = i
+                    line_start_point = new_point
+                current_point = new_point
+            elif line.startswith(TYPE_ID):
+                current_type = line
+        line = Line(gcode_lines[line_start_idx:], self.config, line_start_point, current_point, current_type, enabled)
+        lines.append(line)
+        return gcode_lines[:pre_end_idx], lines, current_point
+
+class Line:
+    def __init__(self, gcode_lines, config, start_point, end_point, type, enabled):
+        self.config = config
+        self.lines = self.process_gcode_lines(gcode_lines, enabled)
+        self.start_point = start_point
+        self.end_point = end_point
+        self.type = type
+        self.enabled = enabled
+
+    def gcode_lines(self):
+        return self.lines
+
+    @property
+    def line_length(self):
+        current_point = self.start_point
+        length = 0
+        for line in self.lines:
+            g1 = parse_line(line)
+            if g1 is not None:
+                new_point = current_point.updated_with_g1_move(g1)
+                length += current_point.xy_dist(new_point)
+        return length
+
+    def add_deretraction(self, position=None):
+        if self.config.retract_len > 0:
+            position = 1 if position is None else position
+            self.lines.insert(position, f"G1 E{gcode_fmt(self.config.retract_len)} F{self.config.retract_speed}")
+
+    def add_retraction(self, position=None):
+        if self.config.retract_len > 0:
+            position = len(self.lines) if position is None else position
+            self.lines.insert(position, f"G1 E-{gcode_fmt(self.config.retract_len)} F{self.config.retract_speed}")
+            position += 1
+            self.lines.insert(position, f"G1 F{self.config.travel_speed}")
+
+    def add_start_feedrate(self):
+        g1 = parse_line(self.lines[0])
+        if g1.f is None:
+            self.lines[0] += f" F{self.config.travel_speed}"
+
+    @staticmethod
+    def type_removed(gcode_lines):
+        return [line for line in gcode_lines if not line.startswith(TYPE_ID)]
+
+    @staticmethod
+    def retractions_removed(gcode_lines):
+        for i in range(-1, max(-5, len(gcode_lines) * -1), -1):
+            line = gcode_lines[i]
+            g1 = parse_line(line)
+            if g1 is not None and g1.x is None and g1.y is None and g1.z is None and g1.e is not None:
+                gcode_lines = gcode_lines[:i]
+                break
+
+        for i in range(0, min(5, len(gcode_lines))):
+            line = gcode_lines[i]
+            g1 = parse_line(line)
+            if g1 is not None and g1.x is None and g1.y is None and g1.z is None and g1.e is not None:
+                del gcode_lines[i]
+                break
+        return gcode_lines
+
+    @classmethod
+    def process_gcode_lines(cls, gcode_lines, enabled):
+        lines = gcode_lines
+        if enabled:
+            lines = cls.type_removed(gcode_lines)
+            lines = cls.retractions_removed(lines)
+        return lines
+
 def parse_line(gcode_line):
     result = g1_line.match(gcode_line)
     if result:
@@ -78,9 +336,12 @@ def store_gcode_lines(gcode_lines, gcode_path):
     gcode_path.write_text(gcode_text)
 
 def get_value_for_id(id, gcode_lines):
+    if isinstance(id, str):
+        id = [id]
     for line in reversed(gcode_lines): # Go reversed because these things are found at the end
-        if line.startswith(id):
-            return line.split("=")[-1].strip()
+        for gcode_id in id:
+            if line.startswith(gcode_id):
+                return line.split("=")[-1].strip()
 
 def gcode_fmt(value, precision=3):
     return f"{value:.{precision}f}".strip("0") if value != 0 else "0.000"
@@ -103,115 +364,11 @@ def get_retraction_count(gcode_lines):
     return retractions
 
 def process_gcode(gcode_lines, wipe_threshold):
-    travel_speed = int(get_value_for_id(TRAVEL_SPEED_ID, gcode_lines)) * 60     # convert to speed/min
-    travel_speed_z = int(get_value_for_id(TRAVEL_SPEED_Z_ID, gcode_lines)) * 60 # convert to speed/min
-    nozzle_diameter = float(get_value_for_id(NOZZLE_DIAMETER_ID, gcode_lines).split(",")[0])
-    if wipe_threshold is None:
-        wipe_threshold = float(get_value_for_id(RETRACT_BEFORE_TRAVEL_ID, gcode_lines).split(",")[0])
-
-    retract_len = float(get_value_for_id(RETRACT_LENGTH_ID, gcode_lines).split(",")[0])
-    retract_speed = int(get_value_for_id(RETRACT_SPEED_ID, gcode_lines).split(",")[0]) * 60 # convert to speed/min
-
-    processed_gcode_lines = []
-
-    i = 0
-
-    current_m204_idx = None
-    current_line_start_idx = None
-
-    layer_change_idx = None
-
-    current_type = None
-    new_type = None
-
-    blip_remover_enabled = False
-    current_line_len = 0
-
-    is_retracted = False
-    remove_next_deretraction = False
-    current_point = Point.undefined()
-    prev_xy_point = Point.undefined()
-    prev_line_end_point = Point.undefined()
-
-    for (orig_idx, line) in enumerate(gcode_lines):
-        processed_gcode_lines.append(line)
-
-        if line == BLIP_REMOVER_ENABLED:
-            blip_remover_enabled = True
-        elif line == BLIP_REMOVER_DISABLED:
-            blip_remover_enabled = False
-            current_point = Point.undefined()
-            prev_xy_point = Point.undefined()
-        elif line == LAYER_CHANGE_ID:
-            layer_change_idx = i
-        elif blip_remover_enabled:
-            g1 = parse_line(line)
-            if g1 is not None:
-                new_point = current_point.updated_with_g1_move(g1)
-
-                if g1.e is not None and current_point.is_xy_equal(new_point):
-                    is_retracted = g1.e < 0
-                    if not is_retracted and remove_next_deretraction:
-                        processed_gcode_lines[i] = "; BLIP_REMOVER REMOVE RETRACTION END"
-                        remove_next_deretraction = False
-
-                if current_point.is_fully_defined:
-                    if g1.e is None and not current_point.is_xy_equal(new_point): # Travel move found
-                        if current_line_len > 0 and current_line_len < nozzle_diameter:
-                            gcode_len = len(processed_gcode_lines)
-                            del processed_gcode_lines[current_line_start_idx:current_m204_idx]
-                            i -= gcode_len - len(processed_gcode_lines)
-                            global lines_removed
-                            lines_removed += 1
-                        else:
-                            prev_line_end_point = current_point
-                            current_line_start_idx = current_m204_idx
-                            current_type = new_type
-                        current_line_len = 0
-
-                        if layer_change_idx > current_line_start_idx:
-                            prev_line_end_point = Point.undefined()
-
-                        if prev_line_end_point.is_fully_defined:
-                            if new_point.xy_dist(prev_line_end_point) > wipe_threshold and not is_retracted:
-                                gcode_len = len(processed_gcode_lines)
-                                processed_gcode_lines.insert(i, f"G1 E-{gcode_fmt(retract_len)} F{retract_speed}")
-                                processed_gcode_lines.append(f"G1 E{gcode_fmt(retract_len)} F{retract_speed}")
-
-                                processed_gcode_lines.insert(i, "; BLIP_REMOVER RETRACTION START")
-                                processed_gcode_lines.append("; BLIP_REMOVER RETRACTION END")
-
-                                i += len(processed_gcode_lines) - gcode_len
-                            elif new_point.xy_dist(prev_line_end_point) <= wipe_threshold and is_retracted:
-                                processed_gcode_lines[i-1] = "; BLIP_REMOVER REMOVE RETRACTION START"
-                                remove_next_deretraction = True
-
-                            if current_type != new_type:
-                                processed_gcode_lines.append(new_type)
-                                i += 1
-
-                    elif g1.e is not None and g1.e > 0 and not current_point.is_xy_equal(new_point): # Extrusion
-                        current_line_len += current_point.xy_dist(new_point)
-
-                if not new_point.is_xy_equal(current_point):
-                    prev_xy_point = current_point
-                current_point = new_point
-            elif line.startswith(M204_ID):
-                current_m204_idx = i
-                if current_line_start_idx is None or layer_change_idx > current_line_start_idx:
-                    prev_line_end_point = current_point
-                    current_line_start_idx = current_m204_idx
-                    if current_type != new_type:
-                        processed_gcode_lines.append(new_type)
-                        i += 1
-            elif line.startswith(TYPE_ID):
-                new_type = line
-        i+=1
-
-    return processed_gcode_lines
+    gcode = GCode(gcode_lines)
+    return gcode.gcode_lines()
 
 def main():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="Removes extrusions that are shorter than the nozzle diameter. Note: Currently, wipes, z-hop, and multiple extruder (or single extruder multi material) prints are not supported.")
     parser.add_argument("file_path", help="The path to the gcode file to process.")
     parser.add_argument("--wipe-threshold", help="When the upcoming travel distance is greater than this, the wipe and hop is added. The default is the 'Minimum travel after retraction' slicer setting.", type=float)
     parser.add_argument("--output-file-path", help="The path to save the processed output to. If not given, the original file is overwrtiten.")
